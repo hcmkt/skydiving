4223bbc1b175cbc44452eb7aa3b273efab6a8db0
hcmkt <hcmkt3634@gmail.com>
2022-11-20 20:34:10 +0900

    refactor: Detach functions and constants

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -1,88 +1,43 @@
-import { useEffect, useState, useRef, createRef } from "react";
-import liff from "@line/liff";
-import axios from "axios";
-import toast, {Toaster} from "react-hot-toast"
+import { useEffect, useState, useRef } from "react";
+import { Toaster } from "react-hot-toast"
 import MultipleChoice from "./MultipleChoice";
 import Vacancy from "./Vacancy";
 import Notification from "./Notification";
 import Submission from "./Submission";
+import { reservationDays, reservationTimes, photographers, notificationTimes } from "./constants";
+import { declareMultipleRefs, initialize, fetchSettings, handleSubmit } from "./functions";
 import "./App.css";
 
 function App() {
-  const declareMultipleRefs = (options, refs) => {
-    options.forEach((_, i) => {
-      refs.current[i] = createRef();
-    });
-  };
-
   const [settings, setSettings] = useState([]);
-
-  const reservationDays = ["月", "火", "水", "木", "金", "土", "日"];
   const reservationDayRefs = useRef([]);
-  declareMultipleRefs(reservationDays, reservationDayRefs);
-
-  const reservationTimes = ["08:15", "10:00", "12:00", "14:00"];
   const reservationTimeRefs = useRef([]);
-  declareMultipleRefs(reservationTimes, reservationTimeRefs);
-
-  const photographers = ["有", "無"];
   const photographerRefs = useRef([]);
-  declareMultipleRefs(photographers, photographerRefs);
-
-  const notificationTimes = [...Array(24).keys()].map(x => String(x).padStart(2, "0") + ":00");
   const notificationTimeRefs = useRef([]);
-  declareMultipleRefs(notificationTimes, notificationTimeRefs);
-
   const vacancyRef = useRef();
   const notificationRef = useRef();
 
-  useEffect(() => {
-    liff
-      .init({
-        liffId: import.meta.env.VITE_LIFF_ID,
-        withLoginOnExternalBrowser: true,
-      })
-      .catch((e) => {
-        toast.error('通信に失敗しました。');
-      });
-  });
+  declareMultipleRefs(reservationDays, reservationDayRefs);
+  declareMultipleRefs(reservationTimes, reservationTimeRefs);
+  declareMultipleRefs(photographers, photographerRefs);
+  declareMultipleRefs(notificationTimes, notificationTimeRefs);
 
-  useEffect(() => {
-    liff.ready.then(() => {
-      const accessToken = liff.getAccessToken();
-      const getSettings = async () => {
-        const response = await axios.get(import.meta.env.VITE_API_URL + "/settings", {params: {'token': accessToken}});
-        setSettings(response.data);
-      }
-      getSettings();
-    });
-  }, []);
+  useEffect(initialize);
+  useEffect(fetchSettings(setSettings), []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = {
-      "accessToken": liff.getAccessToken(),
-      "reservationDays": reservationDays.filter((_, i) => reservationDayRefs.current[i].current.checked),
-      "reservationTimes": reservationTimes.filter((_, i) => reservationTimeRefs.current[i].current.checked),
-      "photographer": photographers.filter((_, i) => photographerRefs.current[i].current.checked),
-      "notificationTimes": notificationTimes.filter((_, i) => notificationTimeRefs.current[i].current.checked),
-      "vacancy": vacancyRef.current.value,
-      "notification": notificationRef.current.checked,
-    };
-    toast.promise(
-      axios.put(import.meta.env.VITE_API_URL + "/update", data),
-      {
-        loading: 'Loading',
-        success: 'Success',
-        error: 'Error',
-      }
-    )
+  const refs = {
+    reservationDayRefs,
+    reservationTimeRefs,
+    photographerRefs,
+    notificationTimeRefs,
+    vacancyRef,
+    notificationRef,
   };
 
   return (
     <div className="text-center my-10">
       <h1 className="text-3xl pb-3">設定</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(refs)}>
         <div className="pb-3">
           <div className="pb-2.5">
             <h2 className="text-2xl pb-2">予約</h2>
