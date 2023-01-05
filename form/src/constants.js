cccb5bf639cb639100aa1a20865a69cd85181df0
hcmkt <hcmkt3634@gmail.com>
2023-01-05 21:31:34 +0900

    feat: Delete form

--- form/src/constants.js
+++
@@ -1,6 +0,0 @@
-const reservationDays = ["月", "火", "水", "木", "金", "土", "日"];
-const reservationTimes = ["08:15", "10:00", "12:00", "14:00"];
-const photographers = ["有", "無"];
-const notificationTimes = [...Array(24).keys()].map(x => String(x).padStart(2, "0") + ":00");
-
-export { reservationDays, reservationTimes, photographers, notificationTimes };
