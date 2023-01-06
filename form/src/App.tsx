f87e238d6169da9d5e9afab47b096c699c844ca0
hcmkt <hcmkt3634@gmail.com>
2023-01-06 19:13:19 +0900

    feat: Update form

--- form/src/App.tsx
+++ form/src/App.tsx
@@ -1,43 +1,20 @@
-import { FC, useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import type { FC } from 'react';
 import liff from '@line/liff';
-import './App.css';
+import { isLocal } from 'domains/lib';
+import From from 'components/ecosystems/Form';
 
 const App: FC = () => {
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
-
   useEffect(() => {
-    liff
-      .init({
+    if (!isLocal()) {
+      void liff.init({
         liffId: import.meta.env.VITE_LIFF_ID,
-      })
-      .then(() => {
-        setMessage('LIFF init succeeded.');
-      })
-      .catch((e: Error) => {
-        setMessage('LIFF init failed.');
-        setError(`${e.stack?.split('\n')[0] ?? ''}`);
+        withLoginOnExternalBrowser: true,
       });
-  });
+    }
+  }, []);
 
-  return (
-    <div className="app">
-      <h1>create-liff-app</h1>
-      {message && <p>{message}</p>}
-      {error && (
-        <p>
-          <code>{error}</code>
-        </p>
-      )}
-      <a
-        href="https://developers.line.biz/ja/docs/liff/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        LIFF Documentation
-      </a>
-    </div>
-  );
+  return <From />;
 };
 
 export default App;
